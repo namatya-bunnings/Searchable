c22aebc6e87758eb6faf6e896770597ef4962508
Niraj Amatya <namatya@bunnings.com.au>
2024-04-27 09:38:48 +1000

    shows error message when the search query is not found.

---
+++ src/components/ErrorMessage/ErrorMessage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./ErrorMessage.css";
+
+const ErrorMessage = () => {
+  return (
+    <div className="error">
+      <p>No Match Found! Please try again.</p>
+    </div>
+  );
+};
+export default ErrorMessage;
