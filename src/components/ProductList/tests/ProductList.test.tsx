ef61d42d65cd1c251877088ed3b47d3fd8ad169d
Niraj Amatya <namatya@bunnings.com.au>
2024-04-28 23:53:37 +1000

    tests added to ProductList

---
+++ src/components/ProductList/tests/ProductList.test.tsx
@@ -0,0 +1,60 @@
+import { render, screen } from "@testing-library/react";
+import ProductList from "../ProductList";
+import { ProductListProps } from "../../ProductList/ProductList.types";
+
+const setup = (props: ProductListProps) => {
+  render(<ProductList {...props} />);
+};
+
+const sampleProps = {
+  productList: [
+    [
+      {
+        category: "Fruits",
+        price: "$1",
+        stocked: true,
+        name: "Apple",
+      },
+      {
+        category: "Fruits",
+        price: "$1",
+        stocked: true,
+        name: "Dragonfruit",
+      },
+      {
+        category: "Fruits",
+        price: "$2",
+        stocked: false,
+        name: "Passionfruit",
+      },
+    ],
+    [
+      {
+        category: "Vegetables",
+        price: "$2",
+        stocked: true,
+        name: "Spinach",
+      },
+      {
+        category: "Vegetables",
+        price: "$4",
+        stocked: false,
+        name: "Pumpkin",
+      },
+      {
+        category: "Vegetables",
+        price: "$1",
+        stocked: true,
+        name: "Peas",
+      },
+    ],
+  ],
+};
+
+describe("ProductList", () => {
+  it("should render product categories correctly", () => {
+    setup(sampleProps);
+    expect(screen.getByText("Fruits")).toBeInTheDocument();
+    expect(screen.getByText("Vegetables")).toBeInTheDocument();
+  });
+});
