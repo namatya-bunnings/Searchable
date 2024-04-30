40b20be296c84b633304c81a7fd2011245ef7115
Niraj Amatya <namatya@bunnings.com.au>
2024-04-30 13:37:38 +1000

    removed table

--- src/components/ProductDetails/ProductDetails.tsx
+++ src/components/ProductDetails/ProductDetails.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Product } from "../ProductList/ProductList.types";
+import "./ProductDetails.css";
 
 const ProductDetails = ({ product }: { product: Product }) => {
   const { name, price } = product;
   return (
-    <tr>
-      <td>{name}</td>
-      <td>{price}</td>
-    </tr>
+    <div className="product-details">
+      <div>{name}</div>
+      <div>{price}</div>
+    </div>
   );
 };
 export default ProductDetails;
